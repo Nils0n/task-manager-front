c5d32efe5e4f8b79be3337e36f1e5e8a919f80f1
nils0n <nilson_jose.v@hotmail.com>
2022-10-17 10:00:33 -0300

    feat: add tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,11 @@
-import { useEffect, useState } from 'react'
-import TaskItem from './components/TaskItem';
-import api from './services/api';
+import Tasks from './components/Tasks';
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      description: "Estudar programação",
-      isCompleted: false
-    },
-    {
-      id: 2,
-      description: "Ler",
-      isCompleted: true
-    },
-  ]);
 
-
-  useEffect(() => {
-
-    const fetchTasks = async () => {
-      try {
-        const { data } = await api.get('tasks');
-        // setTasks(data);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    fetchTasks();
-
-  }, []);
 
   return (
     <>
-      {tasks.map(task => (
-        <TaskItem
-          key={task.id}
-          task={task}
-        />
-      ))}
+      <Tasks />
     </>
   )
 }
---
+++ src/components/Tasks/index.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import TaskItem from "../TaskItem";
+import "./styles.scss";
+
+export default function Tasks() {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            description: "Estudar programação",
+            isCompleted: false
+        },
+        {
+            id: 2,
+            description: "Ler",
+            isCompleted: true
+        },
+    ]);
+
+
+    useEffect(() => {
+
+        const fetchTasks = async () => {
+            try {
+                const { data } = await api.get('tasks');
+                // setTasks(data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        fetchTasks();
+
+    }, []);
+    return (
+        <div className="tasks-container">
+            <h2>Minhas Tarefas</h2>
+            <div className="last-tasks">
+                <h3>Últimas tarefas</h3>
+                <div className="tasks-list">
+                    {tasks.filter(task => task.isCompleted === false).map(lasTask => (
+                        <TaskItem
+                            key={lasTask.id}
+                            task={lasTask}
+                        />
+                    ))}
+                </div>
+            </div>
+            <div className="completed-tasks">
+                <h3>Tarefas Concluídas</h3>
+                <div className="tasks-list">
+                    {tasks.filter(task => task.isCompleted).map(completedTask => (
+                        <TaskItem
+                            key={completedTask.id}
+                            task={completedTask}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
