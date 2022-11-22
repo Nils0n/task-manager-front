12dd79c108c5f9f24c31a8d47e805a4ff083e6b6
nils0n <nilson_jose.v@hotmail.com>
2022-11-22 13:40:04 -0300

    fix: adjust imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 import "./index.scss";
-import Login from "./pages/Login";
 
 
 function App() {
